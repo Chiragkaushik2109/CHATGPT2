72dc246ecd95ee6146f840d0e210f2ad912c2453
Chiragkaushik2109 <kaushikchirag7906@gmail.com>
2023-08-29 11:51:44 +0530

    Initial Commit

---
+++ ChatGPT2/ChatGPT2App.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPT2App.swift
+//  ChatGPT2
+//
+//  Created by Chirag Kaushik on 29/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPT2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
